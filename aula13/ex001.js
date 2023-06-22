364889ed5acf0a93245586a1056e60da46b1a5c7
Gabriel ON <gabrielblackbr1@gmail.com>
2023-06-22 20:56:33 -0300

    Update ex001.js

--- aula13/ex001.js
+++ aula13/ex001.js
@@ -1,5 +1,13 @@
-var c = 1
-while (c < 6){
+var c = 1 // segunda opção
+do {
     console.log('Tudo bem?')
     c++
-}
+} while (c <= 6)
+
+
+
+// var c = 1 // primeira opção
+// while (c <= 6){
+//     console.log('Tudo bem?')
+//     c++
+// }

3cf18dd98434d5831755445544c2b26379a987f7
Gabriel ON <gabrielblackbr1@gmail.com>
2023-06-21 23:45:50 -0300

    bad

---
+++ aula11/ex001.js
@@ -0,0 +1,6 @@
+var vel = 70
+console.log(`A Velocidade do carro é ${vel}km/h`)
+if(vel > 60)[ // condição simples!
+    console.log(`Voce foi MULTADO!`)
+]
+console.log(`Dirija com cinto de segurança!`)

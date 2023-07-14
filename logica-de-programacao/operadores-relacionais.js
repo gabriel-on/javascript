292255b5c140b0cad6640359ae0af2444526fd62
Gabriel ON <gabrielblackbr1@gmail.com>
2023-07-14 20:57:09 -0300

    update

---
+++ logica-de-programacao/operadores-relacionais.js
@@ -0,0 +1,13 @@
+// Igual: ==
+// Diferente: !=
+// Menor: <
+// Maior: >
+// Menor igual: <=
+// Maior igual: >=
+
+let x = 2, y = 4
+Boolean.resultado;
+
+resultado = x == y
+
+console.log(`${x} é igual a ${y}? ${resultado}`)

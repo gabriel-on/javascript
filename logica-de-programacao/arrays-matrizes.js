08a281f55eaa2eeb832442013dfe20650b77ed1e
Gabriel ON <gabrielblackbr1@gmail.com>
2023-07-15 16:18:11 -0300

    Create arrays-matrizes.js

---
+++ logica-de-programacao/arrays-matrizes.js
@@ -0,0 +1,18 @@
+let nomes = [2, 2]
+
+for(let linha = 0; linha < 2; linha++) {
+    for(let coluna = 0; coluna < 2; coluna++) {
+        nomes[linha, coluna]
+    }
+}
+
+for(let linha = 0; linha < 2; linha++) {
+    for(let coluna = 0; coluna < 2; coluna++) {
+        nomes[linha, coluna]
+
+        console.log('s', 't', 'y', 'x')
+    }
+}
+
+// 0.0 0.1
+// 1.0 1.1

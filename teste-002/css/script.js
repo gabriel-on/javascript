8193f5d8ed61ba1340a97ab7358b166229ca49d5
Gabriel ON <gabrielblackbr1@gmail.com>
2023-07-17 18:20:37 -0300

    update

--- teste-002/css/script.js
+++ teste-002/css/script.js
@@ -1,3 +1,21 @@
-// let nome = window.prompt ('qual é seu nome?')
+let menuItem = document.querySelectorAll('.item-menu')
 
-// window.alert('Bem-vindo(a), ' + nome + '!')
+function select(){
+    menuItem.forEach((item) => {
+        item.classList.remove('ativo')
+    })
+    this.classList.add('ativo')
+}
+
+menuItem.forEach((item) => {
+    item.addEventListener('click', select)
+});
+
+// Expandir o menu
+
+let expandir = document.querySelector('#btn-exp')
+let menuLateral = document.querySelector('.menu-lateral')
+
+expandir.addEventListener('click', function(){
+    menuLateral.classList.toggle('expandir')
+})

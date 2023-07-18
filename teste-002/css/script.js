1484c8b9ede649c34c47e317552ec41b6d1e0edc
Gabriel ON <gabrielblackbr1@gmail.com>
2023-07-17 22:49:40 -0300

    update

--- teste-002/css/script.js
+++ teste-002/css/script.js
@@ -6,7 +6,6 @@ function select(){
     })
     this.classList.add('ativo')
 }
-
 menuItem.forEach((item) => {
     item.addEventListener('click', select)
 });
@@ -19,3 +18,26 @@ let menuLateral = document.querySelector('.menu-lateral')
 expandir.addEventListener('click', function(){
     menuLateral.classList.toggle('expandir')
 })
+
+// -------------------------------------------------------
+
+// let mainItem = document.querySelectorAll('.menu-item')
+
+// function selecteD(){
+//     mainItem.forEach((mItem) => {
+//         mItem.classList.remove('ativo2')
+//     })
+//     this.classList.add('ativo2')
+// }
+// mainItem.forEach((mItem) => {
+//     mItem.addEventListener('click', selecteD)
+// });
+
+
+// let mainContainer = document.querySelector('#main-container')
+// let menuMain = document.querySelector('.main')
+
+// menuMain.addEventListener('click', function(){
+//     mainContainer.classList.toggle('mainContainer')
+//     console.log('logo')
+// })

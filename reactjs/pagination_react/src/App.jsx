fb749eb9c72bac2d0f1b91c49f167d5335fc64bd
Gabriel ON <gabrielblackbr1@gmail.com>
2023-10-27 00:59:41 -0300

    pag_v1

---
+++ reactjs/pagination_react/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import Pagination from "./Pagination";
+
+const App = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  return (
+    <div className="container">
+      <Pagination currentPage={currentPage}
+        total={480}
+        limit={20}
+        onPageChange={(page) => setCurrentPage(page)}/>
+    </div>
+  );
+};
+
+export default App;

eb38550f4d3b4789bc271d161d4708617f1b1785
Gabriel ON <gabrielblackbr1@gmail.com>
2023-12-25 14:17:45 -0300

    dev/game - updated

--- reactjs/gg_list_test/gg_list/src/routes/post/Post.jsx
+++ reactjs/gg_list_test/gg_list/src/routes/post/Post.jsx
@@ -1,33 +1,42 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import api from '../../axios/config'
-import { Link } from 'react-router-dom'
-import '../post/Post.css'
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import api from '../../axios/config';
+import { Link } from 'react-router-dom';
+import '../post/Post.css';
 
-const Post = () => {
-
-  const {id} = useParams()
-
-  const [post, setPost] = useState({})
+const GamePage = () => {
+  const { id } = useParams();
+  const [game, setGame] = useState({});
+  const [developer, setDeveloper] = useState({});
 
   useEffect(() => {
+    // Obter detalhes do jogo
     api.get(`/posts/${id}`)
-    .then(response => setPost(response.data))
-    .catch(err => console.log(err))
+      .then(response => setGame(response.data))
+      .catch(err => console.log(err));
 
-  }, [])
+    // Obter detalhes do desenvolvedor
+    api.get(`/developers/${game.developerId}`)
+      .then(response => setDeveloper(response.data))
+      .catch(err => console.log(err));
+  }, [id, game.developerId]);
 
   return (
-    <div className='post'>
-      <h2>{post.title}</h2>
-      <img src={post.img} alt="" />
-      <p>
-        <Link to={`/developers`}>
-          Desenvolvedor
-        </Link>
-      </p>
-    </div>
-  )
-}
+    <div className='game-page'>
+      <div>
+        <h2>{game.title}</h2>
+        <img src={game.img} alt={game.title} />
+        <p>{game.description}</p>
+        
+        {/* Exibir informações sobre o desenvolvedor */}
+        <p>Desenvolvedor: <Link to={`/developers/${developer.id}`}>{developer.name}</Link></p>
 
-export default Post
+        <div className="game-details">
+          <p>Avaliação: {game.rating}/10</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GamePage;

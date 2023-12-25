b9c2def2efa2cf59c7f41f8ffa20acdb9c719fab
Gabriel ON <gabrielblackbr1@gmail.com>
2023-12-25 14:28:29 -0300

    rating - updated

--- reactjs/gg_list_test/gg_list/src/routes/post/Post.jsx
+++ reactjs/gg_list_test/gg_list/src/routes/post/Post.jsx
@@ -8,6 +8,8 @@ const GamePage = () => {
   const { id } = useParams();
   const [game, setGame] = useState({});
   const [developer, setDeveloper] = useState({});
+  const [editing, setEditing] = useState(false);
+  const [newRating, setNewRating] = useState('');
 
   useEffect(() => {
     // Obter detalhes do jogo
@@ -21,18 +23,46 @@ const GamePage = () => {
       .catch(err => console.log(err));
   }, [id, game.developerId]);
 
+  const handleEditRating = () => {
+    // Enviar a nova avaliação para a API (você pode ajustar conforme necessário)
+    api.patch(`/posts/${id}`, { rating: newRating })
+      .then(response => {
+        setGame({ ...game, rating: newRating });
+        setEditing(false);
+      })
+      .catch(err => console.log(err));
+  };
+
   return (
     <div className='game-page'>
       <div>
         <h2>{game.title}</h2>
         <img src={game.img} alt={game.title} />
         <p>{game.description}</p>
-        
+
         {/* Exibir informações sobre o desenvolvedor */}
         <p>Desenvolvedor: <Link to={`/developers/${developer.id}`}>{developer.name}</Link></p>
 
+        {editing ? (
+          // Formulário de edição de avaliação
+          <div>
+            <label>Nova Avaliação:</label>
+            <input
+              type="number"
+              value={newRating}
+              onChange={(e) => setNewRating(e.target.value)}
+            />
+            <button onClick={handleEditRating}>Salvar Avaliação</button>
+          </div>
+        ) : (
+          // Exibir avaliação atual e botão de edição
+          <div>
+            <p>Avaliação: {game.rating}/10</p>
+            <button onClick={() => setEditing(true)}>Editar Avaliação</button>
+          </div>
+        )}
+
         <div className="game-details">
-          <p>Avaliação: {game.rating}/10</p>
         </div>
       </div>
     </div>

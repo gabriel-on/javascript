34592dea123297af4a422e013ab7e7d3919f56a9
Gabriel ON <gabrielblackbr1@gmail.com>
2023-12-26 02:10:29 -0300

    favorite - updated

--- reactjs/gg_list_test/gg_list/src/routes/post/Post.jsx
+++ reactjs/gg_list_test/gg_list/src/routes/post/Post.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import api from '../../axios/config';
 import { Link } from 'react-router-dom';
-import '../post/Post.css';
+import './Post.css'; // Importe o arquivo CSS
 
 const GamePage = () => {
   const { id } = useParams();
@@ -10,9 +10,12 @@ const GamePage = () => {
   const [developer, setDeveloper] = useState({});
   const [editing, setEditing] = useState(false);
   const [newRating, setNewRating] = useState('');
+  const [favorited, setFavorited] = useState(false);
+  const [buttonColor, setButtonColor] = useState(() => {
+    return localStorage.getItem(`buttonColor-${id}`) || 'green';
+  });
 
   useEffect(() => {
-    // Função assíncrona para buscar detalhes do jogo e do desenvolvedor
     const fetchData = async () => {
       try {
         const gameResponse = await api.get(`/posts/${id}`);
@@ -20,38 +23,67 @@ const GamePage = () => {
 
         const developerResponse = await api.get(`/developers/${gameResponse.data.developerId}`);
         setDeveloper(developerResponse.data);
+
+        const editedGameRating = localStorage.getItem('editedGameRating');
+        if (editedGameRating) {
+          setGame(prevGame => ({ ...prevGame, rating: editedGameRating }));
+        }
+
+        const favoritedStatus = localStorage.getItem('favoritedStatus');
+        if (favoritedStatus) {
+          setFavorited(JSON.parse(favoritedStatus));
+        }
       } catch (error) {
         console.log(error);
       }
     };
 
-    fetchData(); // Chamar a função assíncrona
-  }, [id]); // Remover game.developerId da lista de dependências
+    fetchData();
+  }, [id]);
 
-  const handleEditRating = () => {
-    // Enviar a nova avaliação para a API (você pode ajustar conforme necessário)
-    api.patch(`/posts/${id}`, { rating: newRating })
-      .then(response => {
-        setGame({ ...game, rating: newRating });
-        setEditing(false);
-      })
-      .catch(err => console.log(err));
+  const handleEditRating = async () => {
+    try {
+      await api.patch(`/posts/${id}`, { rating: newRating });
+      localStorage.setItem('editedGameRating', newRating);
+
+      setGame(prevGame => ({ ...prevGame, rating: newRating }));
+      setEditing(false);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const handleFavorite = async () => {
+    try {
+      await api.patch(`/posts/${id}`, { favorited: !favorited });
+
+      const newButtonColor = !favorited ? 'green' : 'red';
+      localStorage.setItem(`buttonColor-${id}`, newButtonColor);
+
+      // Utilize uma função de callback para garantir a sincronização do estado
+      setButtonColor((prevColor) => newButtonColor);
+
+      localStorage.setItem('favoritedStatus', JSON.stringify(!favorited));
+      setFavorited(!favorited);
+    } catch (err) {
+      console.error(err);
+    }
   };
 
   const ratingOptions = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
   return (
     <div className='game-page'>
-      <div>
+      <div className="game-details-container">
         <h2>{game.title}</h2>
-        <img src={game.img} alt={game.title} />
+        <img src={game.img} alt={game.title} className="game-image" />
         <p>{game.description}</p>
 
-        {/* Exibir informações sobre o desenvolvedor */}
-        <p>Desenvolvedor: <Link to={`/developers/${developer.id}`}>{developer.name}</Link></p>
+        <p>
+          Desenvolvedor: <Link to={`/developers/${developer.id}`}>{developer.name}</Link>
+        </p>
 
         {editing ? (
-          // Formulário de edição de avaliação com um select
           <div>
             <label>Nova Avaliação:</label>
             <select
@@ -67,13 +99,25 @@ const GamePage = () => {
             <button onClick={handleEditRating}>Salvar Avaliação</button>
           </div>
         ) : (
-          // Exibir avaliação atual e botão de edição
-          <div>
+          <div className="button-container">
             <p>Avaliação: {game.rating}/10</p>
-            <button onClick={() => setEditing(true)}>Editar Avaliação</button>
+            <button
+              onClick={() => setEditing(true)}
+              className="edit-button"
+            >
+              Editar Avaliação
+            </button>
+
+            <button
+              onClick={handleFavorite}
+              className="favorite-button"
+              style={{ backgroundColor: buttonColor, color: 'white' }}
+            >
+              {favorited ? <i className="bi bi-bookmark-heart"></i> : <i className="bi bi-bookmark-heart-fill"></i>}
+            </button>
           </div>
         )}
-
+        
         <div className="game-details">
           <p>Gêneros:</p>
           {game.genres && Array.isArray(game.genres) && (

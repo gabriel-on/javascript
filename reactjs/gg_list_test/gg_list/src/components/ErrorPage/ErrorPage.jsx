cb4612760c58e6156482d9d8c69bb570123afb2c
Gabriel ON <gabrielblackbr1@gmail.com>
2023-12-26 21:08:26 -0300

    updated

--- reactjs/gg_list_test/gg_list/src/components/ErrorPage/ErrorPage.jsx
+++ reactjs/gg_list_test/gg_list/src/components/ErrorPage/ErrorPage.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../ErrorPage/ErrorPage.css'
 
 const ErrorPage = () => {
     return (
-        <div>
+        <div className='error'>
             <h1>Erro 404</h1>
             <p>A página que você está procurando não foi encontrada.</p>
             <Link to={"/"}>
-                <h1>
+                <h2>
                     Retornar à página principal
-                </h1>
+                </h2>
             </Link>
             {/* Adicione outros elementos ou estilos conforme necessário */}
         </div>

78bf9a0f17840c08f73a81cfb4206236ddcd8a0b
Gabriel ON <gabrielblackbr1@gmail.com>
2024-03-30 14:04:17 -0300

    Created Character Counter

---
+++ projetos/contador-de-caracteres/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import CharacterCounter from './Components/CharacterCounter'
+
+function App() {
+
+  return (
+    <>
+      <CharacterCounter/>
+    </>
+  )
+}
+
+export default App

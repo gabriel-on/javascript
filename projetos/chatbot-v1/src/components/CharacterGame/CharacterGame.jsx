fcffa203704d92619a2ff992f24568d88d2b4c45
Gabriel ON <gabrielblackbr1@gmail.com>
2024-04-05 17:00:47 -0300

    update-CharacterGame-v2

--- projetos/chatbot-v1/src/components/CharacterGame/CharacterGame.jsx
+++ projetos/chatbot-v1/src/components/CharacterGame/CharacterGame.jsx
@@ -5,20 +5,20 @@ import { getDatabase, ref, onValue } from 'firebase/database';
 const CharacterGame = ({ userId }) => {
     const { currentUser } = useAuth();
     const [loading, setLoading] = useState(true);
-    const [characters, setCharacters] = useState([]);
+    const [userCharacters, setUserCharacters] = useState([]);
     const [selectedCharacter, setSelectedCharacter] = useState(null);
-    const [characterHealth, setCharacterHealth] = useState(0); // Corrigido para iniciar com 0
-    const [enemy, setEnemy] = useState(null);
-    const [enemyHealth, setEnemyHealth] = useState(0); // Corrigido para iniciar com 0
+    const [characterHealth, setCharacterHealth] = useState(0);
+    const [opponent, setOpponent] = useState(null);
+    const [opponentHealth, setOpponentHealth] = useState(0);
     const [combatLog, setCombatLog] = useState([]);
     const [isPlayerTurn, setIsPlayerTurn] = useState(true);
+    const [isChoosingOpponent, setIsChoosingOpponent] = useState(false);
 
     useEffect(() => {
         if (currentUser) {
             setLoading(false);
-            console.log('userId:', userId);
         }
-    }, [currentUser, userId]);
+    }, [currentUser]);
 
     useEffect(() => {
         if (userId) {
@@ -32,9 +32,7 @@ const CharacterGame = ({ userId }) => {
                         characterId,
                         ...characterData
                     }));
-                    setCharacters(charactersList);
-
-                    console.log('Dados dos personagens:', charactersList);
+                    setUserCharacters(charactersList);
                 } else {
                     console.log("Nenhum personagem encontrado para este usuário.");
                 }
@@ -47,60 +45,66 @@ const CharacterGame = ({ userId }) => {
     }, [userId]);
 
     const selectCharacter = (character) => {
+        if (!character) return;
+
         setSelectedCharacter(character);
-        setCharacterHealth(character.attributes.Vigor * 10); // Corrigido para definir a vida baseada no atributo Vigor
-
-        // Define os atributos do inimigo com pontos pré-definidos
-        const randomEnemy = {
-            name: 'Goblin',
-            health: character.attributes.Vigor * 10, // Vida igual à do personagem selecionado
-            attack: Math.floor(Math.random() * 10) + 5, // Ataque aleatório entre 5 e 14
-            defense: Math.floor(Math.random() * 5) + 1, // Defesa aleatória entre 1 e 5
-            agility: Math.floor(Math.random() * 10) + 1, // Agilidade aleatória entre 1 e 10
-            dexterity: Math.floor(Math.random() * 10) + 1, // Destreza aleatória entre 1 e 10
-            intelligence: Math.floor(Math.random() * 10) + 1, // Inteligência aleatória entre 1 e 10
-            power: Math.floor(Math.random() * 10) + 1, // Poder aleatório entre 1 e 10
-            speed: Math.floor(Math.random() * 10) + 1, // Velocidade aleatória entre 1 e 10
-            vigor: Math.floor(Math.random() * 10) + 1 // Vigor aleatório entre 1 e 10
-        };
-
-        setEnemy(randomEnemy);
-        setEnemyHealth(randomEnemy.health);
-        setIsPlayerTurn(true);
+        setCharacterHealth(character.attributes.Vigor * 10);
+        setIsChoosingOpponent(true); // Permite ao usuário escolher um oponente após selecionar seu personagem
     };
 
+    const selectOpponent = (opponent) => {
+        if (!opponent) return;
 
-    const attackEnemy = () => {
-        if (!selectedCharacter || !enemy || enemyHealth <= 0 || !isPlayerTurn) return;
+        setOpponent(opponent);
+        setOpponentHealth(opponent.attributes.Vigor * 10);
+        setIsChoosingOpponent(false); // Bloqueia a escolha do oponente após selecioná-lo
+        startGame(); // Inicia o jogo após selecionar o oponente
+    };
 
-        const damage = Math.max(0, selectedCharacter.attributes.Força - enemy.defense);
-        setEnemyHealth(prevHealth => Math.max(0, prevHealth - damage));
-        setCombatLog([...combatLog, `You dealt ${damage} damage to ${enemy.name}`]);
-        setIsPlayerTurn(false);
+    const startGame = () => {
+        setIsPlayerTurn(true); // Inicia a partida
+    };
+
+    const attackOpponent = () => {
+        if (!selectedCharacter || !opponent || opponentHealth <= 0 || !isPlayerTurn) return;
+
+        const damage = Math.max(0, selectedCharacter.attributes.Força - opponent.attributes.Defesa);
+        const newOpponentHealth = Math.max(0, opponentHealth - damage);
+        setOpponentHealth(newOpponentHealth);
+        setCombatLog([...combatLog, `You dealt ${damage} damage to ${opponent.characterName}`]);
+
+        if (newOpponentHealth > 0) {
+            setIsPlayerTurn(false); // Troca para o turno do oponente
+        } else {
+            // O oponente foi derrotado, reinicia o jogo
+            restartGame();
+        }
     };
 
     useEffect(() => {
-        if (enemy && !isPlayerTurn) {
-            const enemyAttackTimer = setInterval(() => {
-                if (enemyHealth > 0 && selectedCharacter) {
-                    const damage = Math.max(0, enemy.attack - selectedCharacter.attributes.Defesa);
+        if (opponent && !isPlayerTurn) {
+            // Simula o ataque do oponente depois de um curto período de tempo
+            const opponentAttackTimer = setInterval(() => {
+                if (opponentHealth > 0 && selectedCharacter) {
+                    const damage = Math.max(0, opponent.attributes.Força - selectedCharacter.attributes.Defesa);
                     setCharacterHealth(prevHealth => Math.max(0, prevHealth - damage));
-                    setCombatLog([...combatLog, `${enemy.name} dealt ${damage} damage to you`]);
-                    setIsPlayerTurn(true); // Troca para o turno do jogador após o ataque do inimigo
+                    setCombatLog([...combatLog, `${opponent.characterName} dealt ${damage} damage to you`]);
+                    setIsPlayerTurn(true); // Troca para o turno do jogador após o ataque do oponente
                 }
             }, 2000);
 
-            return () => clearInterval(enemyAttackTimer);
+            return () => clearInterval(opponentAttackTimer);
         }
-    }, [enemy, enemyHealth, selectedCharacter, isPlayerTurn, combatLog]);
+    }, [opponent, opponentHealth, selectedCharacter, isPlayerTurn, combatLog]);
 
     const restartGame = () => {
         setSelectedCharacter(null);
         setCharacterHealth(0);
-        setEnemy(null);
-        setEnemyHealth(0);
+        setOpponent(null);
+        setOpponentHealth(0);
         setCombatLog([]);
-        setIsPlayerTurn(true);
+        setIsPlayerTurn(true); // Reinicia o turno para o jogador
+        setIsChoosingOpponent(false); // Permite ao usuário escolher um novo oponente
     };
 
     if (loading) {
@@ -109,36 +113,54 @@ const CharacterGame = ({ userId }) => {
 
     return (
         <div className="character-game">
-            {selectedCharacter ? (
+            {!selectedCharacter && !isChoosingOpponent ? (
+                <div className="character-list">
+                    <h2>Choose your character:</h2>
+                    <ul>
+                        {userCharacters.map((character, index) => (
+                            <li key={index} onClick={() => selectCharacter(character)}>
+                                {character.characterName}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            ) : (
                 <>
                     <div className="status">
                         <h2>{selectedCharacter.characterName}</h2>
                         <p>Health: {characterHealth}</p>
-                        <p>Attack: {selectedCharacter.attributes.Força}</p>
-                        <p>Defense: {selectedCharacter.attributes.Defesa}</p>
-                        <p>Agility: {selectedCharacter.attributes.Agilidade}</p>
-                        <p>Dexterity: {selectedCharacter.attributes.Destreza}</p>
-                        <p>Intelligence: {selectedCharacter.attributes.Inteligencia}</p>
-                        <p>Power: {selectedCharacter.attributes.Poder}</p>
-                        <p>Speed: {selectedCharacter.attributes.Velocidade}</p>
-                        <p>Vigor: {selectedCharacter.attributes.Vigor}</p>
+                        {Object.entries(selectedCharacter.attributes).map(([key, value]) => (
+                            <div key={key}>
+                                <p>{key}: {value}</p>
+                            </div>
+                        ))}
                     </div>
-                    {enemy && (
+                    {isChoosingOpponent ? (
+                        <div className="character-list">
+                            <h2>Choose your opponent:</h2>
+                            <ul>
+                                {userCharacters
+                                    .filter(character => character.characterId !== selectedCharacter.characterId) // Remove o próprio usuário da lista de oponentes
+                                    .map((opponent, index) => (
+                                        <li key={index} onClick={() => selectOpponent(opponent)}>
+                                            {opponent.characterName}
+                                        </li>
+                                    ))}
+                            </ul>
+                        </div>
+                    ) : (
                         <div className="status">
-                            <h2>{enemy.name}</h2>
-                            <p>Health: {enemyHealth}</p>
-                            <p>Attack: {enemy.attack}</p>
-                            <p>Defense: {enemy.defense}</p>
-                            <p>Agility: {enemy.agility}</p>
-                            <p>Dexterity: {enemy.dexterity}</p>
-                            <p>Intelligence: {enemy.intelligence}</p>
-                            <p>Power: {enemy.power}</p>
-                            <p>Speed: {enemy.speed}</p>
-                            <p>Vigor: {enemy.vigor}</p>
+                            <h2>{opponent.characterName}</h2>
+                            <p>Health: {opponentHealth}</p>
+                            {Object.entries(opponent.attributes).map(([key, value]) => (
+                                <div key={key}>
+                                    <p>{key}: {value}</p>
+                                </div>
+                            ))}
                         </div>
                     )}
 
-                    <button onClick={attackEnemy} disabled={!enemy || enemyHealth <= 0 || !isPlayerTurn}>Attack</button>
+                    <button onClick={attackOpponent} disabled={!opponent || opponentHealth <= 0 || !isPlayerTurn}>Attack</button>
                     <button onClick={restartGame}>Restart</button>
                     <div className="combat-log">
                         <h3>Combat Log</h3>
@@ -149,17 +171,6 @@ const CharacterGame = ({ userId }) => {
                         </ul>
                     </div>
                 </>
-            ) : (
-                <div className="character-list">
-                    <h2>Choose your character:</h2>
-                    <ul>
-                        {characters.map((character, index) => (
-                            <li key={index} onClick={() => selectCharacter(character)}>
-                                {character.characterName}
-                            </li>
-                        ))}
-                    </ul>
-                </div>
             )}
         </div>
     );

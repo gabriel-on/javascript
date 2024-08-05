2a1ddddd87134aff83fbd65580290c9eb1d7db94
Gabriel ON <gabrielblackbr1@gmail.com>
2024-08-05 14:02:05 -0300

    update "error"

--- projetos/link-aggregator/src/pages/ErrorPage/ErrorPage.jsx
+++ projetos/link-aggregator/src/pages/ErrorPage/ErrorPage.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import './ErrorPage.css'; // Importa o CSS
 
 function ErrorPage() {
   return (
-    <div>ErrorPage</div>
-  )
+    <div className="error-page">
+      <h1>404</h1>
+      <p>Página não encontrada</p>
+      <a href="/" className="home-link">Voltar para a página inicial</a>
+    </div>
+  );
 }
 
-export default ErrorPage
+export default ErrorPage;

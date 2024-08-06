fe92ad4cac97212e3fe69a6b4da6b1aeaba11924
Gabriel ON <gabrielblackbr1@gmail.com>
2024-08-05 22:45:29 -0300

    update 2

--- projetos/link-aggregator/src/pages/Home/Home.jsx
+++ projetos/link-aggregator/src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Home.css';
+import { Link } from 'react-router-dom';
 import UserList from '../../components/UserList/UserList';
 
 function Home() {
@@ -13,6 +14,11 @@ function Home() {
         <section className="service-presentation">
           <h2>Sobre o HIGHLINKS</h2>
           <p>Com o HIGHLINKS, você pode organizar e compartilhar todos os seus links importantes em um só lugar. Ideal para influenciadores, criadores de conteúdo e qualquer pessoa que precise gerenciar vários links de maneira fácil e eficiente.</p>
+        </section>
+        <section className="cta-section">
+          <h2>Junte-se a Nós!</h2>
+          <p>Crie sua conta no HIGHLINKS e comece a organizar seus links agora mesmo!</p>
+          <Link to="/register" className="cta-button">Comece Agora</Link>
         </section>
         <UserList />
       </main>
